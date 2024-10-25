f1d026f295630852f8b77a4ada300f4d19292b3e
Mario Maas <mariomaas@gmail.com>
2024-10-25 13:07:20 +0000

    initial commit after fork

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,20 @@
 import numpy as np
 
 def calculate(list):
+    if len(list) < 9:
+        raise ValueError('List must contain nine numbers.')
 
+    list = list[:9]
 
+    a = np.array(list).reshape(3,3)
 
+    calculations = {
+        'mean': [np.mean(a, axis = 0).tolist(), np.mean(a, axis = 1).tolist(), np.mean(a).tolist()],
+        'variance': [np.var(a, axis = 0).tolist(), np.var(a, axis = 1).tolist(), np.var(a).tolist()],
+        'standard deviation': [np.std(a, axis = 0).tolist(), np.std(a, axis = 1).tolist(), np.std(a).tolist()],
+        'max': [np.max(a, axis = 0).tolist(), np.max(a, axis = 1).tolist(), np.max(a).tolist()],
+        'min': [np.min(a, axis = 0).tolist(), np.min(a, axis = 1).tolist(), np.min(a).tolist()],
+        'sum': [np.sum(a, axis = 0).tolist(), np.sum(a, axis = 1).tolist(), np.sum(a).tolist()]
+    }
 
     return calculations
